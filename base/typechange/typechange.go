ba7eb31307588fb1f8cb01a8dfdf624458a263de
吕帅 <9206525+line-year@user.noreply.gitee.com>
2021-10-09 01:42:36 +0800

    first commit

---
+++ base/typechange/typechange.go
@@ -0,0 +1,21 @@
+package typechange
+
+// Int64ToInt TODO
+func Int64ToInt(i int64) int {
+	return 0
+}
+
+// Int64ToString TODO
+func Int64ToString(i int64) string {
+	return ""
+}
+
+// StringToInt64 TODO
+func StringToInt64(s string) int64 {
+	return 0
+}
+
+// StringToInt TODO
+func StringToInt(s string) int {
+	return 0
+}
---
+++ base/types/types.go
@@ -0,0 +1,9 @@
+package types
+
+type M map[string]interface{}
+
+type IntSlice []int
+
+type Int64Slice []int64
+
+type StringSlice []string
