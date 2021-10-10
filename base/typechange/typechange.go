55b18264c7511228127246802708bad22922638c
吕帅 <9206525+line-year@user.noreply.gitee.com>
2021-10-10 23:57:15 +0800

    完善类型转换逻辑

--- base/typechange/typechange.go
+++ base/typechange/typechange.go
@@ -1,21 +1,28 @@
 package typechange
 
-// Int64ToInt TODO
+import "strconv"
+
+// Int64ToInt int64转int
 func Int64ToInt(i int64) int {
-	return 0
+	return StringToInt(Int64ToString(i))
 }
 
-// Int64ToString TODO
+// Int64ToString int64转字符串
 func Int64ToString(i int64) string {
-	return ""
+	return strconv.FormatInt(i, 10)
 }
 
-// StringToInt64 TODO
+// StringToInt64 字符串转int64
 func StringToInt64(s string) int64 {
-	return 0
+	return int64(StringToInt(s))
 }
 
-// StringToInt TODO
+// StringToInt 字符串转int
 func StringToInt(s string) int {
-	return 0
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		//TODO 打印错误
+		return 0
+	}
+	return i
 }
